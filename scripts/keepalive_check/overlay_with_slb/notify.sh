48619ac7a4ea037a4457cb5ef331d983acf96e93
Zongkai LI <onionpiece@163.com>
2020-03-12 12:05:54 +0800

    Add retry to keepalived notify for overlay with slb
    iptables "lock" conflict may cause notify command failed,
    so add a retry for that.

--- scripts/keepalive_check/overlay_with_slb/notify.sh
+++ scripts/keepalive_check/overlay_with_slb/notify.sh
@@ -6,14 +6,40 @@ slb="a.b.c.d"
 # 389 for ldap, 8080 for harbor
 port=389
 if [[ $state == "MASTER" ]]; then
-	drop_rule_idx=`iptables -nvL DOCKER-USER --line-numbers | grep "${slb}.*dpt:${port}" | awk '/DROP/{print $1}'`
+	drop_rule_idx=""
+	for i in {1..6}; do
+		drop_rule_idx=`iptables -nvL DOCKER-USER --line-numbers | grep "${slb}.*dpt:${port}" | awk '/DROP/{print $1}'`
+		if [[ $? -eq 0 ]]; then
+			break
+		fi
+		sleep 1
+	done
 	if [[ ! -z $drop_rule_idx ]]; then
-		iptables -D DOCKER-USER $drop_rule_idx
+		for i in {1..6}; do
+			iptables -D DOCKER-USER $drop_rule_idx
+			if [[ $? -eq 0 ]]; then
+				break
+			fi
+			sleep 1
+		done
 	fi
 else
 	# BACKUP or FAULT(check failed)
-	drop_rule_idx=`iptables -nvL DOCKER-USER --line-numbers | grep "${slb}.*dpt:${port}" | awk '/DROP/{print $1}'`
+	drop_rule_idx=""
+	for i in {1..6}; do
+		drop_rule_idx=`iptables -nvL DOCKER-USER --line-numbers | grep "${slb}.*dpt:${port}" | awk '/DROP/{print $1}'`
+		if [[ $? -eq 0 ]]; then
+			break
+		fi
+		sleep 1
+	done
 	if [[ -z $drop_rule_idx ]]; then
-		iptables -I DOCKER-USER 1 -s $slb -p tcp --dport $port -j DROP
+		for i in {1..6}; do
+			iptables -I DOCKER-USER 1 -s $slb -p tcp --dport $port -j DROP
+			if [[ $? -eq 0 ]]; then
+				break
+			fi
+			sleep 1
+		done
 	fi
 fi
